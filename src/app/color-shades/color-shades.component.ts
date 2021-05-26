5f77ba9895736bbeae9a270cb93436cc78de0444
prabhakaranUI <34186578+prabhakaranUI@users.noreply.github.com>
2021-05-26 12:41:27 +0530

    color-shades 1

--- src/app/color-shades/color-shades.component.ts
+++ src/app/color-shades/color-shades.component.ts
@@ -9,21 +9,46 @@ export class ColorShadesComponent implements OnInit {
 shadesArray: any = [];
 
   constructor() {
-    this.generate();
+    this.hexToRgb('#FF0000','#008000', 100);
   }
 
   ngOnInit(): void {
+
   }
 
-  generate(): void{
-    let r = 255 % 256;
-    let g = 0 % 256;
-    let b = 0 % 256;
-    for (let i = 0; i < 100; i++){
-      this.shadesArray.push({rgbShades: `${r += 5},${g += 5},${b += 5}`});
+  hexToRgb(hexOne, hexTwo, count): void {
+    const colorOne = [ parseInt(hexOne.slice(1, 3), 16), parseInt(hexOne.slice(3, 5), 16), parseInt(hexOne.slice(5, 7), 16)];
+    const colorTwo = [ parseInt(hexTwo.slice(1, 3), 16), parseInt(hexTwo.slice(3, 5), 16), parseInt(hexTwo.slice(5, 7), 16)];
+    this.shadesArray = this.interpolateColors(
+        `rgb(${Object.keys(colorOne).map((k) => colorOne[k]).join(',')})`,
+        `rgb(${Object.keys(colorTwo).map((k) => colorTwo[k]).join(',')})`,
+        count);
+  }
+
+
+// My function to interpolate between two colors completely, returning an array
+  interpolateColors(color1, color2, steps): void {
+    let stepFactor = 1 / (steps - 1);
+        const interpolatedColorArray: any = [];
+
+    color1 = color1.match(/\d+/g).map(Number);
+    color2 = color2.match(/\d+/g).map(Number);
+
+    for (let i = 0; i < steps; i++) {
+      interpolatedColorArray.push(this.interpolateColor(color1, color2, stepFactor * i));
     }
-    console.log(this.shadesArray);
+
+    return interpolatedColorArray;
   }
 
-
+  interpolateColor(color1, color2, factor): void {
+    if (arguments.length < 3) {
+      factor = 0.5;
+    }
+    let result = color1.slice();
+    for (let i = 0; i < 3; i++) {
+      result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+    }
+    return result;
+  }
 }
